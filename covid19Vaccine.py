c5d2a8a888d5fdab2869faa086a34fabd23f647f
Christian Moreno <christianmoreno@Christians-MacBook-Pro.local>
2021-02-11 20:49:52 -0700

    added check for string that states whether or not vaccines are available

--- covid19Vaccine.py
+++ covid19Vaccine.py
@@ -8,9 +8,11 @@ r = session.get(url)
 
 r.html.render(wait=1)
 
-vaccine_div = r.html.find('div.aler', first=True)
-if vaccine_div is None:
-    return false
-
-
-# print(f'{vaccine_div.html}')
+vaccine_div = r.html.find('div.alert', first=True)
+vaccine_strong_element = vaccine_div.find('strong', first = True)
+vaccine_html_string = vaccine_strong_element.html
+substring = "We have exhausted all available vaccine and appointments for this current allotment."
+if substring in vaccine_html_string:
+    print("Found!")
+else:
+    print("Not found!")
